d900c0238b62fe9c3f45e58422513a235d81f2e9
punitrajendra <punitrajendra100@gmail.com>
2026-09-18 20:44:21 +0530

    Connect Express server to MongoDB Atlas

--- server/index.js
+++ server/index.js
@@ -1,6 +1,13 @@
+require('dotenv').config();
 const express = require('express');
+const mongoose = require('mongoose');
+
 const app = express();
 const PORT = 3000;
+
+mongoose.connect(process.env.MONGO_URI)
+  .then(() => console.log('Connected to MongoDB'))
+  .catch((err) => console.error('MongoDB connection error:', err));
 
 app.get('/', (req, res) => {
   res.send('Habit Tracker backend is running!');
